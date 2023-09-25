7739ac615752aad6c8e2188f13b3bc376ac6ade9
sammycosta <arminetah@hotmail.com>
2023-09-25 10:18:45 -0300

    in json format

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
+import json
+
 import requests
 from bs4 import BeautifulSoup
 
-
 tags = ["data+science", "programacao", "web", "banco+de+dados"]
+all_results = []
 
-
-write_file = open("results.txt", "w")
 for i in tags:
     URL = "https://www.learncafe.com/cursos?filter=all&q="+i
     page = requests.get(URL)
-    
+
     soup = BeautifulSoup(page.content, "html.parser")
 
     results = soup.find(id="cursosLista")
 
     courses = results.find_all("a", class_="card")
 
-
     for c in courses:
         hours = c.find("span", class_="card-hours")
         title = c.find("h5", class_="card-title")
 
-        a = "__________________________________________________ \n"
-        a =  a + "Título: "+ title.text + "\n"
-        a = a + "Tempo para concluir: "+ hours.text.strip() + "\n"
-        a = a + "Como acessar: " + c['href'] + "\n"
-        a = a +"__________________________________________________ \n"
-        
-        write_file.write(a)
+        course = {
+            "title": title.text,
+            "hours": hours.text.replace("h", "").strip(),
+            "link": c['href'],
+        }
 
+        all_results.append(course)
 
+result_dict = {"length": len(all_results), "courses": all_results}
+
+with open("results.json", "w", encoding='utf-8') as f:
+    json.dump(result_dict, f, indent=4, ensure_ascii=False)
