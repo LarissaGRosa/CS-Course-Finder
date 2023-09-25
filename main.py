269a16c7e2bffa5ca11325f0ff9dfa477bc49b61
sammycosta <arminetah@hotmail.com>
2023-09-25 12:13:57 -0300

    check all pages, accepted categories and remove duplicates

--- main.py
+++ main.py
@@ -3,36 +3,63 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-tags = ["data+science", "programacao", "web", "banco+de+dados"]
+print("Processando...")
 all_results = []
 
+# Learn Café
+tags = ["data+science", "programacao", "web",
+        "banco+de+dados", "machine+learning"]
+accepted_categories = ["Informática e internet", "Tecnologia da Informação"]
+
+unique_courses = set()
 for i in tags:
     URL = "https://www.learncafe.com/cursos?filter=all&q="+i
-    page = requests.get(URL)
 
-    soup = BeautifulSoup(page.content, "html.parser")
+    while True:
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    results = soup.find(id="cursosLista")
+        results = soup.find(id="cursosLista")
+        courses = results.find_all("a", class_="card")
 
-    courses = results.find_all("a", class_="card")
+        for c in courses:
+            category = c.find("span", class_="card-category")
+            category_list = [c.text for c in category.find_all("span")]
 
-    for c in courses:
-        hours = c.find("span", class_="card-hours")
-        title = c.find("h5", class_="card-title")
-        pricing = c.find("p", class_="card-price").find("b")
-        if pricing.find("span") is not None:
-            pricing.find("span").decompose()
+            if not any(x in category_list for x in accepted_categories):
+                continue
 
-        course = {
-            "title": title.text,
-            "hours": hours.text.replace("h", "").strip(),
-            "link": c['href'],
-            "price": pricing.text.strip()
-        }
+            hours = c.find("span", class_="card-hours")
+            title = c.find("h5", class_="card-title")
 
-        all_results.append(course)
+            pricing = c.find("p", class_="card-price").find("b")
+            if pricing.find("span") is not None:
+                pricing.find("span").decompose()
+
+            course = {
+                "title": title.text,
+                "hours": hours.text.replace("h", "").strip(),
+                "link": c['href'],
+                "price": pricing.text.strip(),
+                "category": tuple(category_list)
+            }
+
+            if tuple(course.values()) not in unique_courses:
+                unique_courses.add(tuple(course.values()))
+                all_results.append(course)
+
+        pagination = soup.find("nav", attrs={"aria-label": "Paginação"})
+        if pagination is not None:
+            nextPage = pagination.find("li", class_="page-next")
+            if nextPage is not None:
+                URL = "https://www.learncafe.com/cursos" + \
+                    nextPage.find("a")['href']
+                continue
+        break
 
 result_dict = {"length": len(all_results), "courses": all_results}
 
 with open("results.json", "w", encoding='utf-8') as f:
     json.dump(result_dict, f, indent=4, ensure_ascii=False)
+
+print("Processo finalizado.")
